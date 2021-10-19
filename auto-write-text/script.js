6ae8db36a6bf469ca60424178526c92d4d17652b
Manoj Kumar <mk161098@gmail.com>
2021-10-19 17:29:42 +0530

    automatic text

---
+++ auto-write-text/script.js
@@ -0,0 +1,15 @@
+const text = 'This is an Automatic text which you are showing on the screen';
+
+let index = 0;
+
+function writeText() {
+    document.body.innerText = text.slice(0, index);
+
+    index++;
+
+    if (index > text.length) {
+        index = 0;
+    }
+}
+
+setInterval(writeText, 100);
